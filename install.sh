fa776958d7a8ec3af230499c84f1a0fa03ed9928
KingEast <imkingeast@gmail.com>
2021-03-23 12:05:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -322,7 +322,7 @@ EOF
     sed -i "s/User=nobody/User=root/;" /etc/systemd/system/xray.service
     systemctl daemon-reload
     ~/.acme.sh/acme.sh  --installcert  -d  ${your_domain}_ecc   \
-        --key-file   /usr/local/etc/xray/cert/private.key \
+        --key-file   /usr/local/etc/xray/cert/$your_domain.key \
         --fullchain-file  /usr/local/etc/xray/cert/fullchain.cer \
         --reloadcmd  "chmod -R 777 /usr/local/etc/xray/cert && systemctl restart xray.service"
 
